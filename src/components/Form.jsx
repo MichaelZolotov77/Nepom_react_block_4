6e0ae23bb0724d1e88b6dfb76d79dda452809707
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-19 21:17:46 +0300

    Создание state в форме

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,28 @@
-import React from "react"
+import React from 'react';
 
 class Form extends React.Component {
+  state = {
+    firstName: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ firstName: event.target.value });
+  };
+
   render() {
+    const { firstName } = this.state;
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          name="firstName"
+          placeholder="FirstName"
+          value={firstName}
+          onChange={this.handleChange}
+        />
       </div>
-    )
+    );
   }
 }
 
-export { Form }
+export { Form };
