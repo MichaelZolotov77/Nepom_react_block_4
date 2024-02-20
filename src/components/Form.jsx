46dc6553757347f589bec63e33d4bcdc5c20c2da
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-20 21:04:25 +0300

    Валидация значений формы

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,6 +10,22 @@ class Form extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  validateName = () => {
+    if (this.state.firstName.length < 5) {
+      alert("Your first name can't be less than 5 letters");
+    }
+  };
+
+  validateEmail = () => {
+    if (
+      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        this.state.email
+      )
+    ) {
+      alert('email is not valid');
+    }
+  };
+
   render() {
     const { firstName, email } = this.state;
     return (
@@ -20,6 +36,7 @@ class Form extends React.Component {
           placeholder="FirstName"
           value={firstName}
           onChange={this.handleChange}
+          onBlur={this.validateName}
         />
         <input
           type="email"
@@ -27,6 +44,7 @@ class Form extends React.Component {
           placeholder="email"
           value={email}
           onChange={this.handleChange}
+          onBlur={this.validateEmail}
         />
       </div>
     );
