b4323898f6263712625c319e7a624c66146826b7
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-21 20:48:10 +0300

    Задание на имитацию отправки формы

--- src/App.js
+++ src/App.js
@@ -7,24 +7,61 @@ export default class App extends React.Component {
     isAgreeWithTerms: false,
   };
 
+  handleEmail = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleCheckbox = (event) => {
+    this.setState({ isAgreeWithTerms: event.target.checked });
+  };
+
+  handleSubmit = () => {
+    const isValidEmail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        this.state.email
+      );
+
+    const isValidCheckbox = this.state.isAgreeWithTerms;
+
+    if (!isValidEmail) {
+      alert('Your email is not valid');
+      return;
+    }
+
+    if (!isValidCheckbox) {
+      alert('You should accept all terms and conditions');
+      return;
+    }
+
+    this.setState({ email: '', isAgreeWithTerms: false });
+    alert('Thank you for subscription');
+  };
+
   render() {
     // TODO: implement component
     const { email, isAgreeWithTerms } = this.state;
 
     return (
       <div>
-        <input type="email" name="email" placeholder="email" value={email} />
+        <input
+          type="email"
+          name="email"
+          placeholder="email"
+          value={email}
+          onChange={this.handleEmail}
+        />
         <br />
         <label>
           <input
             type="checkbox"
             name="isAgreeWithTerms"
             checked={isAgreeWithTerms}
+            onChange={this.handleCheckbox}
           />
           I agree with terms and conditions
         </label>
         <br />
-        <button>Send</button>
+        <button onClick={this.handleSubmit}>Send</button>
       </div>
     );
   }
