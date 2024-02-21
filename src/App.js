ed327fcad3ade0fd917a80ba03b84488e69c2f6a
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-21 19:56:40 +0300

    Исправленный начальный код

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import { Form } from "./components/Form"
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-  )
+// don't change the Component name "App"
+export default class App extends React.Component {
+  state = {
+    email: '',
+    isAgreeWithTerms: false,
+  };
+
+  render() {
+    // TODO: implement component
+    const { email, isAgreeWithTerms } = this.state;
+
+    return (
+      <div>
+        <input type="email" name="email" placeholder="email" value={email} />
+        <br />
+        <label>
+          <input
+            type="checkbox"
+            name="isAgreeWithTerms"
+            checked={isAgreeWithTerms}
+          />
+          I agree with terms and conditions
+        </label>
+        <br />
+        <button>Send</button>
+      </div>
+    );
+  }
 }
-
-export default App
