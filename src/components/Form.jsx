add6b97b61df471f93b103d1f13d4c4afc2f9faa
MichaelZolotov77 <michaelzol@yandex.ru>
2024-02-21 19:39:42 +0300

    Начальный код

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,99 +1,33 @@
 import React from 'react';
 
-class Form extends React.Component {
-  state = {
-    firstName: '',
-    email: '',
-    message: '',
-    select: '',
-    subscription: false,
-    gender: '',
-  };
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  handleCheckboxChange = (event) => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
-
-  validateName = () => {
-    if (this.state.firstName.length < 5) {
-      alert("Your first name can't be less than 5 letters");
-    }
-  };
-
-  validateEmail = () => {
-    if (
-      !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        this.state.email
-      )
-    ) {
-      alert('email is not valid');
-    }
-  };
+// don't change the Component name "App"
+export default class App extends React.Component {
+  constructor() {
+    this.state = {
+      email: '',
+      isAgreeWithTerms: false,
+    };
+  }
 
   render() {
-    const { firstName, email, message, select, subscription, gender } =
-      this.state;
+    // TODO: implement component
+    const { email, isAgreeWithTerms } = this.state;
+
     return (
       <div>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="FirstName"
-          value={firstName}
-          onChange={this.handleChange}
-          onBlur={this.validateName}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="email"
-          value={email}
-          onChange={this.handleChange}
-          onBlur={this.validateEmail}
-        />
-        <br />
-        <textarea name="message" value={message} onChange={this.handleChange} />
-        <br />
-        <select name="select" value={select} onChange={this.handleChange}>
-          <option value="" disabled></option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
+        <input type="email" name="email" placeholder="email" value={email} />
         <br />
         <label>
           <input
             type="checkbox"
-            name="subscription"
-            checked={subscription}
-            onChange={this.handleCheckboxChange}
+            name="isAgreeWithTerms"
+            checked={isAgreeWithTerms}
           />
-          Subscription
+          I agree with terms and conditions
         </label>
         <br />
-        <input
-          type="radio"
-          name="gender"
-          value="male"
-          onChange={this.handleChange}
-          checked={gender === 'male'}
-        />
-        Male
-        <input
-          type="radio"
-          name="gender"
-          value="female"
-          onChange={this.handleChange}
-          checked={gender === 'female'}
-        />
-        Female
+        <button>Send</button>
       </div>
     );
   }
 }
-
-export { Form };
